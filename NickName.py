95b6952e72228136eb73c5741d3e8a9ee90a7fd3
Thomas Zelaya <130874103+tzel28@users.noreply.github.com>
2023-12-02 19:26:22 -0500

    Add files via upload

---
+++ NickName.py
@@ -0,0 +1,9 @@
+#A Simple python program where it takes the input of the users stores it in a variable and combines the varible to output a name. 
+
+print("Welcome to the Nick Name Generator! If you can think it we can name it.\nLets Start..") #print statement
+
+name1=input("What is the name of your favorite anime hero? ") # first input taken
+
+name2 = input("What is the name of your favotire thing to eat? ") # second input taken
+
+print("Awsome awsome.. let me calcautel\n your new nick name is.." + name1 +name2) # conctenate the strings together
